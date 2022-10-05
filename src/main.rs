71a24c4681c15243eb378fd966e71c31e236ef37
João Vitor Cunha Assumpção <joao.assumpo@gmail.com>
2022-10-05 15:09:53 +0000

    Person

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,10 @@
+mod person;
 
-struct Person{
-    name: String,
-    age: i32,
-}
-
-impl Person{
-    fn from(name: &str, age: i32) -> Person {
-        Person{name: name.to_string(), age}
-    }
-
-    fn set_name(&mut self, name: &str) -> &mut Self{
-        self.name = name.to_string();
-        self
-    }
-
-    fn set_age(&mut self, age: i32) -> &mut Self{
-        self.age = age;
-        self
-    }
-}
+use crate::person::*;
 
 fn main() {
     let mut p = Person::from("pedro", 33);
-    println!("name: {} age: {}", p.name, p.age);
+    println!("name: {} age: {}", p.name(), p.age());
     p.set_name("joao").set_age(55);
-    println!("name: {} age: {}", p.name, p.age);
+    println!("name: {} age: {}", p.name(), p.age());
 }
---
+++ src/person.rs
@@ -0,0 +1,35 @@
+///Person struct with 'name' and 'age'.
+pub struct Person{
+    name: String,
+    age: i32,
+}
+
+impl Person{
+    ///Takes &str as 'name' and i32 as 'age'  
+    /// and returns the struct Person with aformentioned elements populated.
+    pub fn from(name: &str, age: i32) -> Person {
+        Person{name: name.to_string(), age}
+    }
+
+    ///Sets the name element of Person.
+    pub fn set_name(&mut self, name: &str) -> &mut Self{
+        self.name = name.to_string();
+        self
+    }
+
+    ///Sets the age element of Person.
+    pub fn set_age(&mut self, age: i32) -> &mut Self{
+        self.age = age;
+        self
+    }
+
+    ///Returns value of 'name'.
+    pub fn name(&self) -> &str {
+        self.name.as_str()
+    }
+
+    ///Returns value of 'age'.
+    pub fn age(&self) -> i32 {
+        self.age
+    }
+}
